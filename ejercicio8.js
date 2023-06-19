a05c585b083bc2374cb19597b44019b72cbe7107
UsuarioM <eiroa007@gmail.com>
2023-06-19 13:29:31 +0200

    ejercicio.jss

---
+++ ejercicio8.js
@@ -0,0 +1,10 @@
+//8.1
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+if (numberOne >= numberTwo) {
+console.log (numberOne);} else {
+console.log (numberTwo);}
+}
+sum (20,2)
+
